fd7c8d71f18a0bf81edb00f37d91411d95a75a43
Akshaya-sai <38995972+Akshaya-sai@users.noreply.github.com>
2018-05-05 08:03:49 +0530

    Create alpha.cpp

---
+++ alpha.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char s;
+	cin>>s;
+	if((s>'a' && s<'z') || (s>'A' && s<'Z')) cout<<"Alphabet";
+	else cout<<"Not an alphabet";
+	return 0;
+}
